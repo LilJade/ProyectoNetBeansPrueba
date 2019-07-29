f2a63f48616180164f84375b16f7793607f5db8a
LilJade09 <josue2duran@hotmail.com>
2019-07-29 11:24:13 -0600

    - Archivo de prueba para aprender a usar GitHub.
    Mi aporte es la clase ProyectoPrueba con las instrucciones de este proyecto.

---
+++ src/proyectoprueba/ProyectoPrueba.java
@@ -0,0 +1,14 @@
+package proyectoprueba;
+
+public class ProyectoPrueba {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hola Mundo");
+        System.out.println("Empiece a crear cualquier cosa dentro de este proyecto. \nLuego vuelvan a subir el proyecto a manera de actualizacion.");
+        System.out.println("La intencion es que cada uno aprenda a manejar lo basico de GitHub. \n"
+                + "Creen formularios, procesos, clases, botones, eventos, lo que quieran dentro de este proyecto y lo suben!"
+                + "\n :) ");
+    }
+    
+}
